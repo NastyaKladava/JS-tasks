93ace4f668d24a683947eafb3019c752d2ccb8fa
NastyaKladava <nstsologub@gmail.com>
2020-04-06 17:06:12 +0300

    dropdown

--- dropdown list/site.js
+++ dropdown list/site.js
@@ -1,20 +1,50 @@
-function keyPressOnInput() {
-    div = document.getElementsByTagName('div');
-    myInput = document.querySelector('.dropInput');
-    myUl = document.querySelector('.dropContent');
-    //myUl.style.display = 'block';
-    myLi = myUl.querySelectorAll('li');
-    arr = myUl.querySelectorAll('li');
-    let span = document.createElement('span');
-    document.body.append(span);
-    //let country = '';
-    
-    for (i = 1; i < arr.length; i++) {
-      //country.innerHTML = arr[i].innerHTML;
-      span.innerHTML = arr[i].innerHTML;
-      //span.innerHTML += arr[i].innerHTML;
-      myInput.innerHTML = span.innerHTML;
-    }
-    
-    console.log(span);
+function showDropContent(){
+    document.querySelector('.dropContent').classList.toggle('show');
   }
+
+  let myButton = document.querySelector('.dropButton');
+  let myInput = document.querySelector('.dropInput');
+  let myContent = document.querySelector('.dropContent');
+  let ul = document.createElement('ul');
+  myContent.append(ul);
+  ul.innerHTML = '';
+  let arr = [
+    'Asia',
+    'Africa',
+    'Europe',
+    'North America',
+    'South America',
+    'Antarctica/Oceania',
+    'Austrelia'
+  ];
+
+  for (let i = 0; i < arr.length; i++) {
+      ul.innerHTML += `<li>${arr[i]}</li>`;
+      
+  };
+
+  function onInputPress() {
+    ul.innerHTML = '';
+    if (myInput.value != '') {
+      let filter = myInput.value.toUpperCase();
+      let matches = arr.filter(function(item) {
+        if (item.startsWith(filter)) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      if (matches.length > 0) {
+        for (let match of matches) {
+          let li = document.createElement('li');
+          li.innerHTML = match;
+          ul.append(li);
+          
+          li.addEventListener('click', function() {
+            myInput.value = li.innerHTML;
+            ul.innerHTML = '';
+          });
+        }
+      }
+    }
+  };
